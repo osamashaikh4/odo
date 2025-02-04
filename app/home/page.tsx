586c2bf1d1bb23e888569da0caf5bdc72cd7f1b4
osama-bp <osama@bitspro.com>
2025-02-05 03:18:11 +0500

    ok

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,31 @@
 import React from "react";
 import Header from "@/components/common/Header";
 import Sidebar from "@/components/common/Sidebar";
+import { Button } from "@heroui/button";
+import { BsPlus } from "react-icons/bs";
 
 export default function page() {
   return (
     <div className="flex h-full">
-      <Sidebar />
+      <Sidebar active="home" />
       <div className="flex flex-col w-0 flex-1">
         <Header />
-        <div></div>
+        <div className="flex flex-col flex-1 relative overflow-auto">
+          <div className="flex flex-col h-full overflow-x-hidden overflow-y-auto">
+            <div className="flex relative items-center justify-between py-10 px-8 bg-[#FAFAFA]">
+              <div className="flex flex-col gap-3">
+                <h2 className="text-4xl font-bold">Welcome, Dave</h2>
+                <p className="text-lg">
+                  Let's create your first shipment together!
+                </p>
+              </div>
+              <Button radius="sm" color="primary">
+                <BsPlus className="h-6 w-6" />
+                Ship Your First Order
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/common/Sidebar.tsx
+++ components/common/Sidebar.tsx
@@ -1,27 +1,110 @@
-import Image from "next/image";
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { cn } from "@/helpers";
 
-export default function Sidebar() {
+interface SidebarProps {
+  active: string;
+}
+
+const SidebarItems = [
+  {
+    label: "Home",
+    value: "home",
+    icon: (
+      <Image src="/assets/icons/home.svg" alt="home" width={24} height={24} />
+    ),
+  },
+  {
+    label: "Shipments",
+    value: "shipments",
+    icon: (
+      <Image
+        src="/assets/icons/shipment.svg"
+        alt="shipment"
+        width={25}
+        height={25}
+      />
+    ),
+  },
+  {
+    label: "Integrations",
+    value: "integrations",
+    icon: (
+      <Image src="/assets/icons/apps.svg" alt="apps" width={26} height={26} />
+    ),
+  },
+  {
+    label: "Shipping Partners",
+    value: "shipping-partners",
+    icon: (
+      <Image src="/assets/icons/truck.svg" alt="truck" width={30} height={30} />
+    ),
+  },
+  {
+    label: "Pickup Location",
+    value: "pickup-location",
+    icon: (
+      <Image
+        src="/assets/icons/location.svg"
+        alt="location"
+        width={24}
+        height={24}
+      />
+    ),
+  },
+];
+
+const Sidebar = ({ active }: SidebarProps) => {
   return (
     <div
       id="sidebar"
-      className="w-[13rem] h-full sticky top-0 z-50 overflow-hidden flex flex-col border-r border-r-gray-200 transition-all duration-300 ease-in-out justify-between"
+      className="w-[217px] h-full sticky top-0 z-50 overflow-hidden flex flex-col border-r border-r-gray-200 transition-all duration-300 ease-in-out justify-between"
     >
       <div
         className="h-full bg-white"
         style={{ maxHeight: "calc(-23rem + 100dvh)" }}
       >
-        <div className="flex items-center justify-between p-4 h-20">
+        <div className="flex items-center justify-between p-4 h-16 relative overflow-hidden">
           <Image
-            className="absolute -top-[65px] -left-[31px]"
+            className="absolute -top-[73px] -left-[34px]"
             src="/assets/images/odo-logo.png"
             alt="odo-logo"
             width={500}
             height={500}
           />
         </div>
+        <div className="relative flex-1 flex flex-col min-h-0 bg-white pt-0">
+          <div className="flex-1 px-2 bg-white divide-y space-y-1">
+            <ul className="space-y-2 pb-2">
+              <li></li>
+              {SidebarItems.map((sidebarItem) => (
+                <li key={sidebarItem.value}>
+                  <Link
+                    href={
+                      sidebarItem.value === "dashboard"
+                        ? "/admin"
+                        : "/admin/" + sidebarItem.value
+                    }
+                    className={cn(
+                      "text-lg text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group",
+                      active === sidebarItem.value ? "bg-gray-100" : ""
+                    )}
+                  >
+                    {sidebarItem.icon}
+                    <span className="ml-3 flex-1 whitespace-nowrap">
+                      {sidebarItem.label}
+                    </span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
       <div className="flex flex-col p-4 z-50 bg-white"></div>
     </div>
   );
-}
+};
+
+export default Sidebar;
